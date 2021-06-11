4f10c4314ab825b819ca9d736c18ba96947ca498
prathameshg121 <ghogarep2000@gmail.com>
2021-06-11 18:19:39 +0530

    basic Notification is added

--- src/components/reminder.jsx
+++ src/components/reminder.jsx
@@ -53,12 +53,38 @@ function Reminder ()
         setTimeout(function initAlarm(){
             console.log(diff)
             play()
+            if(Notification.permission === "granted"){ 
+                prompNotification();
+            }
+            else if(Notification.permission !== "denied")
+            {
+               
+                Notification.requestPermission().then(permission=>{
+                    if(permission === "granted"){
+                        prompNotification();
+                    }
+                       
+                    });
+            }
         } , difference)
 
        }
       
    
     }
+
+    //Notification
+    Notification.requestPermission()
+    function prompNotification (){
+        const notify = new Notification ("Reminder",{
+            body : data + " your Reminder",
+            icon : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHo1oJF_JcPs69Hv6EE0mJNxvEeK_Cgef6St-ubhI5KsCwxt_MHSmAx2m8PkQ2s4qXwbg&usqp=CAU"
+        }
+        )
+    }
+   
+    
+    
     
    
 
