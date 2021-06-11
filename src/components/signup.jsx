0fe277eea1732da55da84e9809c77fd7c2c2bc36
prathameshg121 <ghogarep2000@gmail.com>
2021-06-11 17:42:41 +0530

    sign in is updated

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Button } from '@material-ui/core';
-import { BrowserRouter as Router , Link , Switch , Route } from 'react-router-dom';
+import { BrowserRouter as Router , Link , Switch , Route, useHistory } from 'react-router-dom';
 import Home from './Home';
 //sign in
-export default function signup() {
+export default function Signup() {
+    var history = useHistory();
     return (
         <div>
         <form>
@@ -12,10 +13,7 @@ export default function signup() {
            <input className="inputeplace" type="email" placeholder="email" ></input>
            <input className="inputeplace" type="password" placeholder="password" required="required" ></input> 
            <input className="inputeplace" type="password" placeholder="conform-password" required="required" ></input>
-           <Router>
-               <Link to ="/home"><Button variant="contained" color="primary" type="sumit">Sign up</Button></Link>
-               <Route path="/home" ><Home/></Route>
-           </Router> 
+        <Button variant="contained" color="primary" type="sumit" onClick={()=>{history.push("/login")}}>Sign up</Button>
            
 
            </div>
