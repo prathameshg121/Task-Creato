386f97f5d12bca3385c782d152bb8249c36f3890
prathameshg121 <ghogarep2000@gmail.com>
2021-06-15 10:03:39 +0530

    ui is updated

--- src/components/createNote.jsx
+++ src/components/createNote.jsx
@@ -48,6 +48,7 @@ function increaseSize(){
 
   return (
     <div>
+    <h3 >Add Note</h3>
       <form className="create-note">
        { display?<input name="title"onChange={makeChange} placeholder="Heading" value={data.title} /> :""}
         <textarea name="content" onClick={increaseSize} onChange={makeChange} value ={data.content} placeholder="Discription..." rows={rowValue} />
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -3,9 +3,13 @@ import {Button} from '@material-ui/core';
 import { BrowserRouter as Router , Switch,Route , Link , useHistory} from 'react-router-dom';
 import Home from './Home';
 
-export default function Login() {
+export default function Login(proc) {
     const [islogin, setlogin] = useState(false);
     const histor = useHistory();
+    function goToHome (){
+        proc.checkLogin(true);
+        histor.push("./home")
+    }
 
 
     return (
@@ -16,7 +20,7 @@ export default function Login() {
             <h2>Login</h2>
             <input className="inputeplace" type="email" placeholder="email" ></input>
            <input className="inputeplace" type="password" placeholder="password" required="required"  ></input> 
-          <Button type="submit" variant="contained" color="primary" onClick={ ()=>{histor.push("./home")}} >Login</Button>
+          <Button  type="submit" variant="contained" color="primary" onClick={ goToHome}  >Login</Button>
             </div>
         </form>    
         </div>
